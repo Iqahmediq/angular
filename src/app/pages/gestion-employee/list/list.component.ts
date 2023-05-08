69bd5c206e46dcac99bb1f6f4a3c36281fbfd70c
iqahmediq <belkhiriaahmed617@gmail.com>
2023-05-08 02:38:47 +0100

    first commit

---
+++ src/app/pages/gestion-employee/list/list.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/services/employee.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  public listEmployees: any[] = [];
+  public departments: any[] = [];
+  public selectedEmployee: any = null;
+
+  constructor(private empService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+    this.getDepartments();
+  }
+
+  getEmployees(): void {
+    this.empService.getEmployees().subscribe(
+      (data) => {
+        console.log(data);
+        this.listEmployees.push(...data);
+      },
+      (err) => console.log(err)
+    );
+  }
+  getDepartmentName(id: number): string {
+    const department = this.departments.find(d => d.id === id);
+    return department ? department.departmentName : '';
+  }
+  getDepartments(): void {
+    this.empService.getDepartments().subscribe(
+      data => {
+        console.log(data);
+        this.departments = data;
+      },
+      err => console.log(err)
+    );
+  }
+  openEditForm(employee: any): void {
+    this.selectedEmployee = employee;
+  }
+  closeEditForm(): void {
+    this.selectedEmployee = null;
+  }
+  submitEditForm(): void {
+    this.empService.updateEmployee(this.selectedEmployee.id, this.selectedEmployee )
+      .subscribe(
+        (data) => {
+          console.log(data);
+          
+          // Close the edit form and update the employee in the list
+          this.closeEditForm();
+          const index = this.listEmployees.findIndex(e => e.id === this.selectedEmployee.id);
+          if (index !== -1) {
+            this.listEmployees[index] = data;
+          }
+        },
+        (err) => console.log(err)
+      );
+  }
+  
+
+  deleteEmployee(employee: any): void {
+    // Ask the user for confirmation
+    if (confirm('Are you sure you want to delete this employee?')) {
+      // Send a DELETE request to the API endpoint to delete the employee
+      this.empService.deleteEmployee(employee.id).subscribe(
+        () => {
+          console.log('Employee deleted successfully');
+          // Remove the deleted employee from the listEmployees array
+          const index = this.listEmployees.indexOf(employee);
+          if (index !== -1) {
+            this.listEmployees.splice(index, 1);
+          }
+        },
+        (err) => console.log(err)
+      );
+    }
+  }
+}
